a34eb1acc2570d06eb3e393f1932c32ea0371ce3
Soulball <navarrete.de16@tectijuana.edu.mx>
2021-01-11 14:02:22 -0800

    Segundo commit

--- main.dart
+++ main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Log in',
       theme: ThemeData(
-          brightness: Brightness.dark,
+          brightness: Brightness.light,
           primaryColor: Colors.redAccent[700],
           accentColor: Colors.redAccent[200]),
       initialRoute: "/",
